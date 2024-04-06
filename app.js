fa5095e5b5905cda8487504cabcaa3c18375cb50
ManuelRea05 <agustinrea105@gmail.com>
2024-04-06 14:43:27 -0300

    galeria y boton whap

--- app.js
+++ app.js
@@ -14,3 +14,32 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+// boton whatsapp
+document.addEventListener("DOMContentLoaded", function() {
+    function showWhatsAppButton() {
+        var whatsappButton = document.querySelector(".btn-wsp");
+        whatsappButton.style.display = "block";
+    }
+    // Función para ocultar el botón de WhatsApp cuando se activa el menú responsive
+    function hideWhatsAppButton() {
+        var nav = document.getElementById("nav");
+        var whatsappButton = document.querySelector(".btn-wsp");
+        if (nav.classList.contains("responsive")) {
+            whatsappButton.style.display = "none";
+        } else {
+            showWhatsAppButton();
+        }
+    }
+    // Llama a la función al cargar la página
+    hideWhatsAppButton();
+
+    // Evento para escuchar cuando se hace clic en el botón del menú
+    document.getElementById("icono-nav").addEventListener("click", function() {
+        hideWhatsAppButton();
+    });
+
+    var navLinks = document.querySelectorAll("#nav a");
+    navLinks.forEach(function(link) {
+        link.addEventListener("click", showWhatsAppButton);
+    });
+});
